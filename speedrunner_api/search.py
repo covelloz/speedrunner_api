b57d3ccea226863a948941387bec9ebecac9c2fc
Michael Covello <covelloz.michael@gmail.com>
2019-04-30 22:52:50 -0700

    Finish and cleanup search module

--- speedrunner_api/search.py
+++ speedrunner_api/search.py
@@ -5,20 +5,34 @@ class Search(Database):
     def __init__(self):
         super().__init__()
 
-    def all_games(self):
+    def _exec_query(self, sql, params=None):
+        """
+        Params:
+            sql:= <str>
+            params:= <None>/<str>/list<str>
+
+        Returns:
+            response:= list<dict<variant>> (json serializable)
+        """
         conn = super().make_connection()
-        sql = 'SELECT game_id, game FROM Games;'
-        results = conn.execute(sql).fetchall()
+        if params is None:
+            results = conn.execute(sql)
+        else:
+            results = conn.execute(sql, params)
+        keys = results.keys()
+        response = [dict(zip(keys, result)) for result in results.fetchall()]
         super().destroy_connection(conn)
-        keys = ('game_id', 'game')
-        response = [dict(zip(keys, result)) for result in results]
+        return response
+
+    def all_games(self):
+        sql = 'SELECT game_id, game FROM Games;'
+        response = self._exec_query(sql)
         return response
 
     def games_in_category(self, category):
-        conn = super().make_connection()
         sql = '''
             SELECT
-                g.game_id, g.game, c.category_id, c.category
+                g.game_id, g.game
             FROM Games g
             INNER JOIN GameCategoryMap gcm
                 ON g.game_id = gcm.game_id
@@ -26,17 +40,50 @@ class Search(Database):
                 ON gcm.category_id = c.category_id
             WHERE c.category = %s;
         '''
-        results = conn.execute(sql, category).fetchall()
-        super().destroy_connection(conn)
-        keys = ('game_id', 'game', 'category_id', 'category')
-        response = [dict(zip(keys, result)) for result in results]
+        response = self._exec_query(sql, params=category)
         return response
 
     def top_speedruns_by_game(self, game):
-        pass
-
-    def top_speedruns_by_category(self, category):
-        pass
+        sql = '''
+            SELECT sr.speedrun_id, p.player, sr.duration
+            FROM SpeedRuns sr
+            INNER JOIN Games g
+                ON sr.game_id = g.game_id
+            INNER JOIN Players p
+                ON sr.player_id = p.player_id
+            WHERE g.game = %s
+            ORDER BY sr.duration ASC
+            LIMIT 10;
+        '''
+        response = self._exec_query(sql, params=game)
+        return response
 
     def all_speedruns_by_player(self, player):
-        pass
+        sql = '''
+            SELECT sr.speedrun_id, g.game, sr.duration
+            FROM SpeedRuns sr
+            INNER JOIN Games g
+                ON sr.game_id = g.game_id
+            INNER JOIN Players p
+                ON sr.player_id = p.player_id
+            WHERE p.player = %s;
+        '''
+        response = self._exec_query(sql, params=player)
+        return response
+
+    def top_players_by_category(self, category):
+        sql = '''
+            SELECT p.player_id, p.player
+            FROM SpeedRuns sr
+            INNER JOIN GameCategoryMap gcm
+                ON sr.game_id = gcm.game_id
+            INNER JOIN Categories c
+                ON gcm.category_id = c.category_id
+            INNER JOIN Players p
+                ON sr.player_id = p.player_id
+            WHERE c.category = %s
+            ORDER BY sr.duration ASC
+            LIMIT 10;
+        '''
+        response = self._exec_query(sql, params=category)
+        return response
